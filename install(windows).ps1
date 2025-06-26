498ef259aa9499123e3bcaec04665188fd01d7af
mathyc0de <mateushackrs7@gmail.com>
2025-06-26 19:50:51 -0300

    ✨ feat: add script to download libs

---
+++ install(windows).ps1
@@ -0,0 +1,9 @@
+if (-not (Test-Path -Path "lib" -PathType Container)) {
+    New-Item -ItemType Directory -Path "lib" | Out-Null
+}
+Set-Location "lib"
+git clone https://github.com/kikito/bump.lua
+git clone https://github.com/kikito/anim8
+git clone https://github.com/Yonaba/Jumper
+git clone https://github.com/vrld/hump
+Set-Location "../"
