9ea81f95a824a5cbcb87b7bd7db1a947ab5982e1
mathyc0de <mateushackrs7@gmail.com>
2025-06-27 01:35:46 -0300

    ✨ feat: config file

---
+++ conf.lua
@@ -0,0 +1,17 @@
+function love.conf(t)
+	t.title = "nome do jogo"
+	t.window.icon = "icon.png"
+	t.version = "11.5"
+	t.window.width = 800
+	t.window.height = 600
+	t.console = true
+
+
+	-- t.window.minheight = 500
+	-- t.window.minwidth = 500
+	-- t.window.resizable = true
+	-- t.window.borderless = true
+	-- t.window.vsync = 1
+	-- t.window.display = 2
+	-- t.window.fullscreen = true
+end
